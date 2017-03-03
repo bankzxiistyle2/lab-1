c87bc538ca00cfe4e2070aaa775c7708ad0fa498
Kanthakarn Kalawong <58160698@my.buu.ac.th>
2017-03-04 01:30:45 +0700

    move to preproject

---
+++ miniproject2/index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        
+    </head>
+    <body>
+<?php
+session_start();
+
+if(isset($_POST['submitname'])){ //check login button
+    if(isset($_POST['name'])){
+        $_SESSION['name']=$_POST['name'];
+        echo '<script type="text/javascript">alert("login complete");</script>';
+    }else{
+        echo '<script type="text/javascript">alert("error to login");</script>';
+    }
+}
+
+if(!isset($_SESSION['name'])){ //login page 
+    ?>
+
+<div id='login'>
+     <form action='index.php' method='post'>
+     inter name :
+     <input type='text' name='name' id='name'>
+     <input type='submit' name='submitname' id='submitname'>
+     
+     </form>
+     </div>
+
+    <?php 
+}else{//in the chat room
+ ?>
+<div id='paber'>
+    <div id='header'><?php echo 'Name : '+$_SESSION['name'] ?>
+    </div>
+
+
+</div>
+<?php
+}
+?>
+    </body>
+</html>
